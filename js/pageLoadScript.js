9cca3c3414e7270e948460af37f0b0148bd358e5
Zachary Whale <Zach.whale@gmail.com>
2018-11-30 10:43:45 -0500

    Set up Cookie to delegate auto refresh set up auto refresh to 10 minutes Set up option menu added users

--- js/pageLoadScript.js
+++ js/pageLoadScript.js
@@ -1,41 +1,41 @@
 
 
-  var onlineStreamer = new Array();
-  var offlineStreamer = new Array();
+var onlineStreamer = new Array();
+var offlineStreamer = new Array();
 
-  function loopStreamers()
-  {
+function loopStreamers()
+{
+  $.ajax({
+    type: "GET",
+    url: "pages/streamers.txt",
+    success: getStreamerChannel,
+    error: function(jqXHR, textStatus, error){
+      console.log(error);
+    }
+  });
+}
+
+function getStreamerChannel(incomingData)
+{
+  var streamerNames = new Array();
+
+  streamerNames = incomingData.split("\n");
+
+
+  streamerNames.map(streamerNames => {
+    const twitchChannel =  'https://api.twitch.tv/kraken/channels/' + streamerNames,
+    twitchStreams =  'https://api.twitch.tv/kraken/streams/' + streamerNames,
+    twitchUsers = 'https://api.twitch.tv/kraken/users/' + streamerNames;
+
+    $.ajaxSetup({headers:{"Client-ID": "eu8zhc3k49ubec2brcpc824bhetvrk"}});
+
     $.ajax({
       type: "GET",
-      url: "pages/streamers.txt",
-      success: getStreamerChannel,
-      error: function(jqXHR, textStatus, error){
-        console.log(error);
-      }
-    });
-  }
-
-  function getStreamerChannel(incomingData)
-  {
-    var streamerNames = new Array();
-
-    streamerNames = incomingData.split("\n");
-
-
-    streamerNames.map(streamerNames => {
-      const twitchChannel =  'https://api.twitch.tv/kraken/channels/' + streamerNames,
-      twitchStreams =  'https://api.twitch.tv/kraken/streams/' + streamerNames,
-      twitchUsers = 'https://api.twitch.tv/kraken/users/' + streamerNames;
-
-      $.ajaxSetup({headers:{"Client-ID": "eu8zhc3k49ubec2brcpc824bhetvrk"}});
-
-      $.ajax({
-        type: "GET",
-        url: twitchChannel,
-        success: function(data) {
-          getStreamerStreams(data, streamerNames, twitchStreams, twitchUsers);
-        },
-        error: function(jqXHR, textStatus, error){console.log(error);}
+      url: twitchChannel,
+      success: function(data) {
+        getStreamerStreams(data, streamerNames, twitchStreams, twitchUsers);
+      },
+      error: function(jqXHR, textStatus, error){console.log(error);}
 
     });
 
@@ -64,40 +64,40 @@ function getStreamerStreams(data, namesData, tsl, tul)
     },
     error: function(jqXHR, textStatus, error){console.log(error);}
 
-});
+  });
 }
 
 function getStreamerUser(incomingData, namesData, cd, tul)
 {
-      var streamerNames = namesData;
-      var channelData = cd;
-      var twitchUserLink = tul;
-      var streamData = incomingData;
+  var streamerNames = namesData;
+  var channelData = cd;
+  var twitchUserLink = tul;
+  var streamData = incomingData;
 
-      $.ajaxSetup({headers:{"Client-ID": "eu8zhc3k49ubec2brcpc824bhetvrk"}});
+  $.ajaxSetup({headers:{"Client-ID": "eu8zhc3k49ubec2brcpc824bhetvrk"}});
 
-      $.ajax({
-        type: "GET",
-        url: twitchUserLink,
-        success: function(data)
-        {
-          //console.log(data);
+  $.ajax({
+    type: "GET",
+    url: twitchUserLink,
+    success: function(data)
+    {
+      //console.log(data);
 
-          //drawOnlineStreamers();
-          //drawOfflineStreamers();
+      //drawOnlineStreamers();
+      //drawOfflineStreamers();
 
-          if(!streamData.stream)
-          {
+      if(!streamData.stream)
+      {
 
-            pushOfflineStreamer(data, streamerNames, channelData);
-          }else {
-            pushOnlineStreamer(data, streamData, streamerNames, channelData);
-          }
+        pushOfflineStreamer(data, streamerNames, channelData);
+      }else {
+        pushOnlineStreamer(data, streamData, streamerNames, channelData);
+      }
 
-        },
-        error: function(jqXHR, textStatus, error){console.log(error);}
+    },
+    error: function(jqXHR, textStatus, error){console.log(error);}
 
-    });
+  });
 
 
 }
@@ -112,9 +112,9 @@ function pushOnlineStreamer(data, sD, sN, cD)
 
 
   var activeStreamer = {
-  channelData: channelData,
-  streamData: streamData.stream,
-  userData: userData
+    channelData: channelData,
+    streamData: streamData.stream,
+    userData: userData
   };
   onlineStreamer.push(activeStreamer);
 }
@@ -125,12 +125,12 @@ function pushOfflineStreamer(data, sN, cD)
   var channelData = cD;
   var userData = data;
 
-    var nonActiveStreamer = {
-      channelData: channelData,
-      userData: userData
-      };
-    offlineStreamer.push(nonActiveStreamer);
-    //printDataUserOffline(nonActiveStreamer);
+  var nonActiveStreamer = {
+    channelData: channelData,
+    userData: userData
+  };
+  offlineStreamer.push(nonActiveStreamer);
+  //printDataUserOffline(nonActiveStreamer);
 
 }
 
@@ -154,7 +154,7 @@ function printDataUserOnline(activeStreamer)
 
 
   $('#streamerOnline').append(
-    `<div class="streamer">
+    `<div id="${channelObject.display_name.toLowerCase()}" class="streamerCard">
     <div class="streamer_name"><b>${channelObject.display_name}</b></div>
     <div class="streamer_logo"><a href=${channelObject.url} target="_blank"><img class="user_image" src=${channelObject.logo}></img></a></div>
     <div class="streamer_views"><img src="content/viewers.png" id="viewers"> ${streamObject.viewers}</img></div>
@@ -186,7 +186,7 @@ function printDataUserOffline(nonActiveStreamer)
   }
 
   $('#streamerOffline').append(
-    `<div class="streamer_offline">
+    `<div id="${channelObject.display_name.toLowerCase()}" class="streamerCardOffline">
     <div class="streamer_name_offline"><b>${channelObject.display_name}</b></div>
     <div class="streamer_logo_offline"><a href=${channelObject.url} target="_blank"><img class="user_image" src=${channelObject.logo}></img></a></div>
     <div class="streamer_description_offline">${userBio}</div>
@@ -195,35 +195,100 @@ function printDataUserOffline(nonActiveStreamer)
   );
 }
 
-  $(document).ready(loopStreamers);
-  $(document).ajaxStop(function()
+function searchBarUpdate()
+{
+    if($("offlineStreamerSearch").val != "")
+    {
+      $(".streamerCardOffline").show();
+      $(".streamerCardoffline").not('[id*=' + $('#offlineStreamerSearch').val().toLowerCase() + ']').hide();
+      $('.streamerCardOffline [id*=' + $('#offlineStreamerSearch').val().toLowerCase() + ']').show();
+
+    }
+
+  }
+
+  function openPanel()
+  {
+    $("#navWindow").css("width", "25%");
+    $(".overlay-content").show();
+
+  }
+
+  function closePanel()
+  {
+    $(".overlay-content").hide();
+    $("#navWindow").css("width", "0px");
+  }
+
+  function refresh()
+  {
+    if($("#refreshToggle").is(":checked"))
+    {
+      window.location.reload(true);
+    }
+
+  }
+
+  function cookieSwap()
+  {
+      Cookies.remove("refreshCookie");
+      var checkedResult = false;
+      if($("#refreshToggle").is(":checked"))
+      {
+        checkedResult = true;
+      }
+      Cookies("refreshCookie", checkedResult);
+
+  }
+
+  function cookieLoad()
+  {
+    var refreshCookieValue = false;
+    if(Cookies.get("refreshCookie") == "true")
+    {
+      refreshCookieValue = true;
+    }
+    $("#refreshToggle").prop("checked", refreshCookieValue);
+
+
+  }
+
+
+$(document).ready(loopStreamers);
+
+$(document).ajaxStop(function()
 {
   offlineStreamer.sort(function(a, b)
-{
-  if(a.channelData.display_name < b.channelData.display_name)
   {
-    return -1;
-  }
-  if(a.channelData.display_name > b.channelData.display_name)
-  {
-    return 1;
-  }
-  return 0;
-});
+    if(a.channelData.display_name < b.channelData.display_name)
+    {
+      return -1;
+    }
+    if(a.channelData.display_name > b.channelData.display_name)
+    {
+      return 1;
+    }
+    return 0;
+  });
 
   onlineStreamer.sort(function(a, b)
-{
-  if(a.streamData.viewers < b.streamData.viewers)
   {
-    return 1;
-  }
-  if(a.streamData.viewers > b.streamData.viewers)
-  {
-    return -1;
-  }
-  return 0;
-});
+    if(a.streamData.viewers < b.streamData.viewers)
+    {
+      return 1;
+    }
+    if(a.streamData.viewers > b.streamData.viewers)
+    {
+      return -1;
+    }
+    return 0;
+  });
 
+if(onlineStreamer.length == 0)
+{
+  $("#onlineStreamerHeader").append("<b>No Streamers Online</b>");
+}else{
+    $("#onlineStreamerHeader").append("<b>Hot Potatoes</b>");
   for(i in onlineStreamer)
   {
     printDataUserOnline(onlineStreamer[i]);
@@ -239,10 +304,37 @@ function printDataUserOffline(nonActiveStreamer)
     player.setVolume(0);
     //console.log(onlineStreamer[i].streamData.viewers)
   }
+}
+if(offlineStreamerHeader.length == 0)
+{
+  $("#offlineStreamerHeader").append("<b>No Spudz around here<b>")
+}else{
+    $("#offlineStreamerHeader").append("<b>Spudz<b>")
+    for(i in offlineStreamer)
+    {
+      printDataUserOffline(offlineStreamer[i]);
+    }
+}
 
-  for(i in offlineStreamer)
-  {
-    printDataUserOffline(offlineStreamer[i]);
-  }
+});
 
-})
+$(document).ready(function() {
+
+
+
+  cookieLoad()
+
+  $("#refreshToggle").change(cookieSwap);
+
+  $("#offlineStreamerSearch").on("input", searchBarUpdate);
+  $("#optionsButton").click(openPanel);
+  $("#closeOverlayButton").click(closePanel);
+  setInterval(function() {
+    refresh()
+  }, 600000);
+
+
+});
+
+
+
